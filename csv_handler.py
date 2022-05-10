e620114e0be9c24581ec68eacc5b8af1c7209d21
Al Matty <al@almatty.com>
2022-05-09 21:19:09 -0500

    Started data preparation

--- csv_handler.py
+++ csv_handler.py
@@ -13,3 +13,30 @@ with open(in_csv) as infile:
     data = data.rename(columns = {
     'Submit a Link to your tweet, video or article': 'Link'
     })
+df = data.copy()
+
+# Sort out doubles
+df = df.drop_duplicates(subset='Link')
+
+# Filter out submissions with invalid links
+contains_status = df['Link'].str.contains('status')
+contains_twitter = df['Link'].str.contains('twitter.com')
+df = df[contains_status & contains_twitter]
+
+# Grab Tweet ID out of link and add as new column
+df['Tweet ID'] = df['Link'].str.extract('(?<=status/)(\d{19})', expand=True)
+
+# Grab Twitter handle and add as new column, rearrange column order
+df['User'] = df['Link'].str.extract(
+    '(?<=twitter\.com/)(.+?)(?=/status)', expand=True
+    )
+rearranged = [
+    'User', 'Tweet ID', 'Retweets', 'Views',
+    'Status', 'Comments', 'Follower Points',
+    'Retweet Points'
+    ]
+df = df[rearranged]
+
+
+# Add column: tweet type (str)
+df['Tweet Tpye'] = df['Tweet ID'].apply(orig_quote_or_rt)
