bc8c0b61725b6ceb561adee0fef14d5fed7bb138
Al Matty <al@almatty.com>
2022-05-20 12:07:15 -0500

    Added verbosity for debugging

--- csv_handler.py
+++ csv_handler.py
@@ -1,9 +1,15 @@
 '''
 This file encapsulates file and data handling.
 '''
+
 import os
 import pandas as pd
 from functions_twitter import *
+
+
+
+
+
 
 
 def load_csv(in_csv, sep=','):
@@ -32,22 +38,27 @@ def fill_missing_data(df):
     df = data.copy()
 
     # Sort out doubles
+    print('\nSorting out doubles...')
     df = df.drop_duplicates(subset='Link')
 
     # Filter out submissions with invalid links
+    print('\nFiltering out invalid links...')
     contains_status = df['Link'].str.contains('status')
     contains_twitter = df['Link'].str.contains('twitter.com')
     df = df[contains_status & contains_twitter]
 
     # Grab Tweet ID out of link and add as new column
+    print('\nExtracting Tweet ID using regex...')
     df['Tweet ID'] = df['Link'].str.extract('(?<=status/)(\d{19})', expand=True)
 
     # Grab Twitter handle and add as new column
+    print('\nExtracting Twitter handle using regex...')
     df['User'] = df['Link'].str.extract(
         '(?<=twitter\.com/)(.+?)(?=/status)', expand=True
         )
 
     # Rearrange column order
+    print('\nRearranging column order...')
     rearranged = [
         'User', 'Tweet ID', 'Retweets', 'Views',
         'Status', 'Comments', 'Follower Points',
@@ -56,9 +67,11 @@ def fill_missing_data(df):
     df = df[rearranged]
 
     # Add column: tweet type (str)
+    print('\nDetermining tweet type (original, quote, RT, or nan (suspended or too old))...')
     df['Tweet Tpye'] = df['Tweet ID'].apply(orig_quote_or_rt)
 
     # Add column: tweet preview
+    print('\nAdding column: Tweet preview...')
     def get_preview(_id, n):
         return get_text(_id)[:n]
     df['Tweet Preview'] = df['Tweet ID'].apply(
@@ -66,21 +79,25 @@ def fill_missing_data(df):
         )
 
     # Add column: user id
+    print('\nQuerying for user ID and adding as new column (get_user_id())...')
     df['User ID'] = df['Tweet ID'].apply(get_user_id)
 
     # Add column: n_followers per user
+    print('\nQuerying for follower count and adding as new column (get_followers_count())...')
     df['Followers'] = df['User ID'].apply(get_followers_count)
 
     # Add column: number of retweets for submitted tweet
+    print('\nQuerying for retweet count and adding as new column (get_retweet_count())...')
     df['Retweets'] = df['Tweet ID'].apply(get_retweet_count)
 
     # Drop duplicates (entries pointing to the same tweet)
+    print('\nDropping rows containing the same Tweet ID...')
     df = df.drop_duplicates('Tweet ID')
 
     return df
 
 
-def save_csv(df, out_path, sort_by=col):
+def save_csv(df, out_path, sort_by='User'):
     '''
     Saves DataFrame with specified column and row order to disk.
     '''
@@ -89,7 +106,7 @@ def save_csv(df, out_path, sort_by=col):
         'Retweet Points', 'Tweet Preview'
         ]
     out_df = df[cols]
-    #out_df.rename(columns={'Tweet Content':'Tweet Preview'}, inplace=True)
+    out_df.rename(columns={'Tweet Content':'Tweet Preview'}, inplace=True)  # was commented out!
     out_df = out_df.sort_values(col, ascending=False)
     out_df.to_csv(out_path, index=False)
     return out_df
