c00af93855adf5cbe44e2297d96f874023dcf6bf
Al Matty <al@almatty.com>
2022-05-20 15:33:44 -0500

    Added functionality to filter out users suspended by Twitter (most likely bots)

--- csv_handler.py
+++ csv_handler.py
@@ -1,3 +1,4 @@
+# csv_handler.py
 '''
 This file encapsulates file and data handling.
 '''
@@ -38,27 +39,27 @@ def fill_missing_data(df):
     df = data.copy()
 
     # Sort out doubles
-    print('\nSorting out doubles...')
+    print('\n\tSorting out doubles...')
     df = df.drop_duplicates(subset='Link')
 
     # Filter out submissions with invalid links
-    print('\nFiltering out invalid links...')
+    print('\n\tFiltering out invalid links...')
     contains_status = df['Link'].str.contains('status')
     contains_twitter = df['Link'].str.contains('twitter.com')
     df = df[contains_status & contains_twitter]
 
     # Grab Tweet ID out of link and add as new column
-    print('\nExtracting Tweet ID using regex...')
+    print('\n\tExtracting Tweet ID using regex...')
     df['Tweet ID'] = df['Link'].str.extract('(?<=status/)(\d{19})', expand=True)
 
     # Grab Twitter handle and add as new column
-    print('\nExtracting Twitter handle using regex...')
+    print('\n\tExtracting Twitter handle using regex...')
     df['User'] = df['Link'].str.extract(
         '(?<=twitter\.com/)(.+?)(?=/status)', expand=True
         )
 
     # Rearrange column order
-    print('\nRearranging column order...')
+    print('\n\tRearranging column order...')
     rearranged = [
         'User', 'Tweet ID', 'Retweets', 'Views',
         'Status', 'Comments', 'Follower Points',
@@ -67,11 +68,11 @@ def fill_missing_data(df):
     df = df[rearranged]
 
     # Add column: tweet type (str)
-    print('\nDetermining tweet type (original, quote, RT, or nan (suspended or too old))...')
+    print('\n\tDetermining tweet type (original, quote, RT, or nan (suspended or too old))...')
     df['Tweet Tpye'] = df['Tweet ID'].apply(orig_quote_or_rt)
 
     # Add column: tweet preview
-    print('\nAdding column: Tweet preview...')
+    print('\n\tAdding column: Tweet preview...')
     def get_preview(_id, n):
         return get_text(_id)[:n]
     df['Tweet Preview'] = df['Tweet ID'].apply(
@@ -79,8 +80,16 @@ def fill_missing_data(df):
         )
 
     # Add column: user id
-    print('\nQuerying for user ID and adding as new column (get_user_id())...')
+    print('\n\tQuerying for user ID and adding as new column (get_user_id())...')
     df['User ID'] = df['Tweet ID'].apply(get_user_id)
+
+    # Remove suspended users (User ID nan triggered error in next block)
+    print('\n\tFiltering out suspended users (User ID = nan)...')
+    suspended = df.loc[df['User ID'].isnull()]
+    df = df.dropna(subset=['User ID'])
+    print('These users have been suspended by Twitter and have been filtered out:\n')
+    user_ids_d = suspended[['User', 'User ID']].set_index('User').to_dict()['User ID']
+    prettyprint(user_ids_d, 'Twitter Handle', 'User ID')
 
     # Add column: n_followers per user
     print('\nQuerying for follower count and adding as new column (get_followers_count())...')
